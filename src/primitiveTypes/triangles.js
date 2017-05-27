0b89487e92060b0d2cb878aae42e0f00065c12e9
Stefan Nieke <info@niekes.com>
2017-05-28 01:49:19 +0200

    add new primitiveTypes

---
+++ src/primitiveTypes/triangles.js
@@ -0,0 +1,7 @@
+import {rotateRxRyRz}   from '../rotation.js';
+import {project}        from '../projection.js';
+
+export function triangles(data, projection, alpha, beta, gamma, origin, scale, distance){
+    for (var i = data.length - 1; i >= 0; i--) {}
+    return data;
+}
