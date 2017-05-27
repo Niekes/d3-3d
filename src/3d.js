f3dbe9946d82974f9d668f5095959e8352e49e55
Stefan Nieke <info@niekes.com>
2017-05-28 01:34:20 +0200

    add tabs as spaces

--- src/3d.js
+++ src/3d.js
@@ -4,7 +4,7 @@ import {lines} 		from './primitiveTypes/lines.js';
 
 export default function() {
 
-	var ortho           = projections.ortho,
+    var ortho           = projections.ortho,
         persp           = projections.persp,
         projection      = ortho,
         origin          = [0, 0],
@@ -32,33 +32,33 @@ export default function() {
         return arguments.length ? (projection = _, _3d) : projection;
     };
 
-	_3d.origin = function(_){
+    _3d.origin = function(_){
         return arguments.length ? (origin = _, _3d) : origin;
-	};
+    };
 
-	_3d.scale = function(_){
+    _3d.scale = function(_){
         return arguments.length ? (scale = _, _3d) : scale;
-	};
+    };
 
-	_3d.distance = function(_){
+    _3d.distance = function(_){
         return arguments.length ? (distance = _, _3d) : distance;
-	};
+    };
 
-	_3d.rotateZ = function(_){
+    _3d.rotateZ = function(_){
         return arguments.length ? (alpha = _, _3d) : alpha;
-	};
+    };
 
-	_3d.rotateY = function(_){
+    _3d.rotateY = function(_){
         return arguments.length ? (beta = _, _3d) : beta;
-	};
+    };
 
-	_3d.rotateX = function(_){
+    _3d.rotateX = function(_){
         return arguments.length ? (gamma = _, _3d) : gamma;
-	};
+    };
 
-	_3d.primitiveType = function(_){
+    _3d.primitiveType = function(_){
         return arguments.length ? (primitiveType = _, _3d) : primitiveType;
-	};
+    };
 
     return _3d;
 }
