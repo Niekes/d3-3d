0fc67c5d76bd93d3ea8b909e5afc5384c7025006
Stefan Nieke <info@niekes.com>
2017-05-28 01:33:02 +0200

    add tabs as spaces

--- src/3d.js
+++ src/3d.js
@@ -6,59 +6,59 @@ export default function() {
 
 	var ortho           = projections.ortho,
         persp           = projections.persp,
-        projection 		= ortho,
-		origin 			= [0, 0],
-		scale 			= 1,
-		distance 		= 1,
-		alpha 			= 0,
-		beta  			= 0,
-		gamma 			= 0,
-		primitiveType	= 'POINTS',
-		primitiveTypes 	= {
-			POINTS 			: points,
-			LINES 			: lines,
-			// LINES_LOOP 		: linesLoop,
-			// LINES_STRIP 	: linesStrip,
-			// TRIANGLES 		: triangles,
-			// TRIANGLES_STRIP : trianglesStrip,
-			// TRIANGLES_FAN	: trianglesFan
+        projection      = ortho,
+        origin          = [0, 0],
+        scale           = 1,
+        distance        = 1,
+        alpha           = 0,
+        beta            = 0,
+        gamma           = 0,
+        primitiveType   = 'POINTS',
+        primitiveTypes  = {
+        POINTS 			: points,
+        LINES 			: lines,
+        // LINES_LOOP : linesLoop,
+        // LINES_STRIP: linesStrip,
+        // TRIANGLES : triangles,
+        // TRIANGLES_STRIP : trianglesStrip,
+        // TRIANGLES_FAN : trianglesFan
 		};
 
 	function _3d(data){
-		return primitiveTypes[primitiveType](data, projection, alpha, beta, gamma, origin, scale, distance);
+        return primitiveTypes[primitiveType](data, projection, alpha, beta, gamma, origin, scale, distance);
 	}
 
 	_3d.projection = function(_){
-		return arguments.length ? (projection = _, _3d) : projection;
+        return arguments.length ? (projection = _, _3d) : projection;
 	};
 
 	_3d.origin = function(_){
-		return arguments.length ? (origin = _, _3d) : origin;
+        return arguments.length ? (origin = _, _3d) : origin;
 	};
 
 	_3d.scale = function(_){
-		return arguments.length ? (scale = _, _3d) : scale;
+        return arguments.length ? (scale = _, _3d) : scale;
 	};
 
 	_3d.distance = function(_){
-		return arguments.length ? (distance = _, _3d) : distance;
+        return arguments.length ? (distance = _, _3d) : distance;
 	};
 
 	_3d.rotateZ = function(_){
-		return arguments.length ? (alpha = _, _3d) : alpha;
+        return arguments.length ? (alpha = _, _3d) : alpha;
 	};
 
 	_3d.rotateY = function(_){
-		return arguments.length ? (beta = _, _3d) : beta;
+        return arguments.length ? (beta = _, _3d) : beta;
 	};
 
 	_3d.rotateX = function(_){
-		return arguments.length ? (gamma = _, _3d) : gamma;
+        return arguments.length ? (gamma = _, _3d) : gamma;
 	};
 
 	_3d.primitiveType = function(_){
-		return arguments.length ? (primitiveType = _, _3d) : primitiveType;
+        return arguments.length ? (primitiveType = _, _3d) : primitiveType;
 	};
 
-	return _3d;
+    return _3d;
 }
--- src/primitiveTypes/lines.js
+++ src/primitiveTypes/lines.js
@@ -1,11 +1,7 @@
-import {rotateRxRyRz} 	from '../rotation.js';
-import {project} 		from '../projection.js';
+import {rotateRxRyRz}   from '../rotation.js';
+import {project}        from '../projection.js';
 
 export function lines(data, projection, alpha, beta, gamma, origin, scale, distance){
-	for (var i = data.length - 1; i >= 0; i--) {
-		// var p  		= data[i];
-		// p.rotated 	= rotateRxRyRz({x : p.x, y : p.y, z : p.z}, alpha, beta, gamma);
-		// p.projected = project(p.rotated, projection, origin, scale, distance);
-	}
-	return data;
+    for (var i = data.length - 1; i >= 0; i--) {}
+    return data;
 }
