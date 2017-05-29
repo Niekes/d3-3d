c64445880c23bc5a7aaf746c8c3be59fd214217a
Stefan Nieke <info@niekes.com>
2017-05-29 22:17:04 +0200

    add length calculation to lines

--- src/primitiveTypes/lines.js
+++ src/primitiveTypes/lines.js
@@ -4,13 +4,18 @@ import {project}        from '../projection.js';
 export function lines(data, projection, alpha, beta, gamma, origin, scale, distance){
     for (var i = data.length - 1; i >= 0; i--) {
         var line      = data[i];
+
         var p1        = line[0];
         var p2        = line[1];
+
         p1.rotated    = rotateRxRyRz({x : p1.x, y : p1.y, z : p1.z}, alpha, beta, gamma);
         p2.rotated    = rotateRxRyRz({x : p2.x, y : p2.y, z : p2.z}, alpha, beta, gamma);
-        line.midPoint = {x: (p1.x + p2.x)/2, y: (p1.y + p2.y)/2, z: (p1.z + p2.z)/2};
+
         p1.projected  = project(p1.rotated, projection, origin, scale, distance);
         p2.projected  = project(p2.rotated, projection, origin, scale, distance);
+
+        line.lng      = Math.sqrt(Math.pow(p2.rotated.x - p1.rotated.x, 2) + Math.pow(p2.rotated.y - p1.rotated.y, 2) + Math.pow(p2.rotated.z - p1.rotated.z, 2));
+        line.midPoint = {x: (p1.x + p2.x)/2, y: (p1.y + p2.y)/2, z: (p1.z + p2.z)/2};
     }
     return data;
 }
--- test/3d-test.js
+++ test/3d-test.js
@@ -75,6 +75,22 @@ tape('mid point calculation for lines', function(test){
 	test.end();
 });
 
+tape('length calculation for lines', function(test){
+	var _3d = d3._3d().primitiveType('LINES');
+	var data = [
+		[{x:  0, y:   0, z:   0}, {x:   -1, y:    0, z:    0}],
+		[{x:  0, y:   0, z:   0}, {x:    0, y:    0, z:    1}],
+		[{x:  0, y:   0, z:   0}, {x:    0, y:    1, z:    0}],
+	];
+	var line0 = _3d(data)[0];
+	var line1 = _3d(data)[1];
+	var line2 = _3d(data)[2];
+	test.deepEqual(line0.lng, 1);
+	test.deepEqual(line1.lng, 1);
+	test.deepEqual(line2.lng, 1);
+	test.end();
+});
+
 // tape('rotate zero point along x axis by 180°', function(test){
 // 	var data = [{x: 0, y: 0, z: 0}];
 // 	var _3d = d3._3d().rotateX(Math.PI);
