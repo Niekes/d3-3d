f2a34f1876e807e499486383e922bb2ceb75f466
Stefan Nieke <info@niekes.com>
2017-05-30 00:16:04 +0200

    remove 3d d3.line()

--- src/draw.js
+++ src/draw.js
@@ -1,8 +1,3 @@
-import shape from 'd3-shape';
-
 export function drawLines(d){
-	return shape.line()([
-		[ d[0].projected.x, d[0].projected.y ],
-		[ d[1].projected.x, d[1].projected.y ],
-	]);
+	return 'M' + d[0].projected.x + ',' + d[0].projected.y + 'L' + d[1].projected.x + ',' + d[1].projected.y;
 }
