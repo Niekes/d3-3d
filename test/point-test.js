165578403051974cf089c07f6b6a7e37818a004a
Niekes <info@niekes.com>
2017-06-09 13:05:26 +0200

    add point draw function test

--- test/point-test.js
+++ test/point-test.js
@@ -1,2 +1,8 @@
 var tape = require('tape');
 var d3   = require('../');
+
+tape('points don\'t have draw function', function(test){
+    var _3d = d3._3d();
+    test.equal(_3d.draw(), undefined);
+    test.end();
+});
