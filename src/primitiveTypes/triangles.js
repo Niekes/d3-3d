55a3997d01c7b18beb232c88bb9dfb6fc0dea03f
Niekes <info@niekes.com>
2017-06-13 22:33:28 +0200

    convert tabs to spaces

--- src/primitiveTypes/triangles.js
+++ src/primitiveTypes/triangles.js
@@ -6,22 +6,22 @@ export function triangles(data, options, point, angles){
 
     for (var i = data.length - 1; i >= 0; i--) {
 
-        var tri         = data[i];
+        var tri      = data[i];
 
-        var p1          = tri[0];
-        var p2          = tri[1];
-        var p3          = tri[2];
+        var p1       = tri[0];
+        var p2       = tri[1];
+        var p3       = tri[2];
 
-        p1.rotated      = rotateRxRyRz({x : point.x(p1), y : point.y(p1), z : point.z(p1)}, angles);
-        p2.rotated      = rotateRxRyRz({x : point.x(p2), y : point.y(p2), z : point.z(p2)}, angles);
-        p3.rotated      = rotateRxRyRz({x : point.x(p3), y : point.y(p3), z : point.z(p3)}, angles);
+        p1.rotated   = rotateRxRyRz({x : point.x(p1), y : point.y(p1), z : point.z(p1)}, angles);
+        p2.rotated   = rotateRxRyRz({x : point.x(p2), y : point.y(p2), z : point.z(p2)}, angles);
+        p3.rotated   = rotateRxRyRz({x : point.x(p3), y : point.y(p3), z : point.z(p3)}, angles);
 
-        p1.projected    = project(p1.rotated, options);
-        p2.projected    = project(p2.rotated, options);
-        p3.projected    = project(p3.rotated, options);
+        p1.projected = project(p1.rotated, options);
+        p2.projected = project(p2.rotated, options);
+        p3.projected = project(p3.rotated, options);
 
-        tri.cw          = clockwise(tri);
-        tri.centroid    = { x: (p1.rotated.x + p2.rotated.x + p3.rotated.x)/3, y: (p1.rotated.y + p2.rotated.y + p3.rotated.y)/3, z: (p1.rotated.z + p2.rotated.z + p3.rotated.z)/3};
+        tri.cw       = clockwise(tri);
+        tri.centroid = { x: (p1.rotated.x + p2.rotated.x + p3.rotated.x)/3, y: (p1.rotated.y + p2.rotated.y + p3.rotated.y)/3, z: (p1.rotated.z + p2.rotated.z + p3.rotated.z)/3};
     }
     return data;
 }
