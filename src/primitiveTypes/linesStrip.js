1fdbe0174a58d0ebe522db7489be4c00b91bcedb
Niekes <info@niekes.com>
2017-06-13 18:10:40 +0200

    fix issue with line_strip

--- src/primitiveTypes/linesStrip.js
+++ src/primitiveTypes/linesStrip.js
@@ -2,11 +2,18 @@ import {rotateRxRyRz}   from '../rotation.js';
 import {project}        from '../projection.js';
 
 export function linesStrip(data, options, point, angles){
+
     for (var i = data.length - 1; i >= 0; i--) {
-        var p       = data[i];
-        p.rotated   = rotateRxRyRz({x : point.x(p), y : point.y(p), z : point.z(p)}, angles);
-        p.centroid  = p.rotated;
-        p.projected = project(p.rotated, options);
+
+        var l = data[i];
+
+        for (var j = l.length - 1; j >= 0; j--) {
+            var p = l[j];
+            p.rotated   = rotateRxRyRz({x : point.x(p), y : point.y(p), z : point.z(p)}, angles);
+            p.centroid  = p.rotated;
+            p.projected = project(p.rotated, options);
+        }
     }
+
     return data;
 }
--- test/lines-strip-test.js
+++ test/lines-strip-test.js
@@ -4,10 +4,8 @@ var d3   = require('../');
 tape('line strip test', function(test){
 	var _3d = d3._3d().primitiveType('LINES_STRIP');
 	var data = [
-		[0,1,2],
-		[3,4,5],
-		[6,7,8],
+		[[0,1,2],[3,4,5],[6,7,8]]
 	];
-	test.deepEqual(_3d.draw(_3d(data)), 'M6,-7L3,-4L0,-1');
+	test.deepEqual(_3d.draw(_3d(data)[0]), 'M6,-7L3,-4L0,-1');
 	test.end();
 });
