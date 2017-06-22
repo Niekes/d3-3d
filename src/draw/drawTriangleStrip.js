2251bc6a6733153c5f4f4155091639ff9cf2f034
Niekes <info@niekes.com>
2017-06-22 23:11:08 +0200

    add triangle strip

--- src/draw/drawTriangleStrip.js
+++ src/draw/drawTriangleStrip.js
@@ -1,3 +1,18 @@
-export function drawTriangleStrip(d){
-	return 'M';
+export function drawTriangleStrip(triangleStrip){
+
+    var path = '';
+
+    for (var i = triangleStrip.length - 1; i >= 2; i--) {
+
+        var j = i - 1;
+        var k = j - 1;
+
+        var v0 = triangleStrip[i].projected;
+        var v1 = triangleStrip[j].projected;
+        var v2 = triangleStrip[k].projected;
+
+        path += 'M' + v0.x + ',' + v0.y + 'L' + v1.x + ',' + v1.y + 'L' + v2.x + ',' + v2.y + 'Z';
+    }
+
+	return path;
 }
