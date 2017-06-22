7d1b363e7785921b5bf9d95c3c79b9d7403f86c7
Niekes <info@niekes.com>
2017-06-22 16:21:31 +0200

    add centroid()

--- src/primitiveTypes/lines.js
+++ src/primitiveTypes/lines.js
@@ -10,7 +10,6 @@ export function lines(data, options, point, angles){
 
         var p1        = line[0];
         var p2        = line[1];
-        var n         = line.length;
 
         p1.rotated    = rotateRxRyRz({x : point.x(p1), y : point.y(p1), z : point.z(p1)}, angles);
         p2.rotated    = rotateRxRyRz({x : point.x(p2), y : point.y(p2), z : point.z(p2)}, angles);
@@ -18,7 +17,7 @@ export function lines(data, options, point, angles){
         p1.projected  = project(p1.rotated, options);
         p2.projected  = project(p2.rotated, options);
 
-        line.centroid = {x: (p1.x + p2.x)/n, y: (p1.y + p2.y)/n, z: (p1.z + p2.z)/n};
+        line.centroid = centroid(line);
     }
     return data;
 }
