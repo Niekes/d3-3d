383c890981cc8b1dd611cebe15a0b1c49b4b2339
Niekes <info@niekes.com>
2017-06-22 23:11:32 +0200

    rename lne to path

--- src/draw/drawLineStrip.js
+++ src/draw/drawLineStrip.js
@@ -1,9 +1,9 @@
 export function drawLineStrip(lineStrip){
     var lastPoint = lineStrip[lineStrip.length - 1];
-    var line = 'M' + lastPoint.projected.x + ',' + lastPoint.projected.y;
+    var path = 'M' + lastPoint.projected.x + ',' + lastPoint.projected.y;
     for (var i = lineStrip.length - 2; i >= 0; i--) {
         var p = lineStrip[i].projected;
-        line += 'L' + p.x + ',' + p.y;
+        path += 'L' + p.x + ',' + p.y;
     }
-    return line;
+    return path;
 }
