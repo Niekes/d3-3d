4ad84ba52eb3bd718563eebcd73b43a4aeabf6f7
Niekes <info@niekes.com>
2017-06-22 11:42:01 +0200

    markup

--- src/projection.js
+++ src/projection.js
@@ -1,6 +1,6 @@
 export function project(d, options){
     return {
-        x: options.origin[0] + options.scale * d.x,
-        y: options.origin[1] + options.scale * d.y
+        x:  (options.origin[0] + options.scale * d.x),
+        y: -(options.origin[1] + options.scale * d.y)
     };
 }
