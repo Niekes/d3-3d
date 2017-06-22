8b826427b4aaa6f9054331573c500936c7d77aa1
Niekes <info@niekes.com>
2017-06-22 11:41:54 +0200

    rename vars

--- src/clockwise.js
+++ src/clockwise.js
@@ -5,11 +5,11 @@ export function clockwise(polygon) {
     _polygon.push(_polygon[0]);
 
     for (var i = polygon.length - 1; i >= 0; i--) {
-
+        var j  = i + 1;
         var p1 = _polygon[i].rotated;
-        var p2 = _polygon[i + 1].rotated;
+        var p2 = _polygon[j].rotated;
 
-        sum += (p2.x - p1.x)*(p2.y + p1.y);
+        sum += (p2.x - p1.x) * (p2.y + p1.y);
     }
 
     return sum > 0 ? true : false;
