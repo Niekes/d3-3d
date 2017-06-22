8834c7b800b5299e325f90ceccec42c0edda82dc
Niekes <info@niekes.com>
2017-06-22 16:22:11 +0200

    add quad tests

--- test/quads-test.js
+++ test/quads-test.js
@@ -9,3 +9,12 @@ tape('quads\' centroid calculation is correct', function(test){
     test.deepEqual(_3d(data)[0].centroid, { x: 0, y: 0, z: 0.5 });
     test.end();
 });
+
+tape('draw function of \'quads\' draws correctly', function(test){
+    var _3d = d3._3d().primitiveType('QUADS');
+    var data = [
+        [[5,0,2],[6,4,1],[4,5,8],[1,5,9]]
+    ];
+    test.deepEqual(_3d.draw(_3d(data)[0]), 'M5,0L6,-4L4,-5L1,-5Z');
+    test.end();
+});
