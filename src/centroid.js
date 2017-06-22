d2d975b1624987fc81b59b3da80a0e59e727ab04
Niekes <info@niekes.com>
2017-06-22 11:40:40 +0200

    add centroid file

---
+++ src/centroid.js
@@ -0,0 +1,17 @@
+export function centroid(polygon){
+
+    var _x = 0, _y = 0, _z = 0;
+    var _n = polygon.length;
+
+    for (var i = _n - 1; i >= 0; i--) {
+        _x += polygon[i].rotated.x;
+        _y += polygon[i].rotated.y;
+        _z += polygon[i].rotated.z;
+    }
+
+    return {
+        x: _x/_n,
+        y: _y/_n,
+        z: _z/_n,
+    };
+}
