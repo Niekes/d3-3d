98b9a9b3cadae0dfe62e532035f66a5d981aced3
Niekes <info@niekes.com>
2017-06-26 20:16:18 +0200

    add perspective projection

--- src/projection.js
+++ src/projection.js
@@ -3,4 +3,11 @@ export function project(d, options){
         x: options.origin[0] + options.scale *  d.x,
         y: options.origin[1] + options.scale * -d.y
     };
+
+    // if(projection === projections.persp){
+    //     return {
+    //         x: origin[0] + scale *  d.x / (d.z + distance),
+    //         y: origin[1] + scale * -d.y / (d.z + distance)
+    //     };
+    // }
 }
