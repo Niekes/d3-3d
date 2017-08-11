25b1fb1ed9c99dfb0910415a3eb5c6ae3e39bfa1
niekes <info@niekes.com>
2017-08-11 21:13:57 +0200

    indent

--- src/point.js
+++ src/point.js
@@ -1,11 +1,11 @@
 export function x(p) {
-  return p[0];
+    return p[0];
 }
 
 export function y(p) {
-  return p[1];
+    return p[1];
 }
 
 export function z(p) {
-  return p[2];
+    return p[2];
 }
