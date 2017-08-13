96815fe1ccacf72b1a8f0ff0961977b91383c038
niekes <info@niekes.com>
2017-08-13 02:06:58 +0200

    remove drawCube coz not need

--- src/draw/drawCube.js
+++
@@ -1,3 +0,0 @@
-export function drawCube(d){
-	return 'M' + d[0].projected.x + ',' + d[0].projected.y + 'L' + d[1].projected.x + ',' + d[1].projected.y + 'L' + d[2].projected.x + ',' + d[2].projected.y + 'L' + d[3].projected.x + ',' + d[3].projected.y + 'Z';
-}
