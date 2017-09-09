0b8c2b33efce0ab23522b5d1c77883c4ae79c048
niekes <info@niekes.com>
2017-09-09 21:45:17 +0200

    extend grid for non quadratic grids

--- src/primitiveShapes/gridPlane.js
+++ src/primitiveShapes/gridPlane.js
@@ -5,13 +5,14 @@ import {point as pt}    from './point';
 export function gridPlane(grid, options, point, angles){
 
     var points = pt(grid, options, point, angles);
-    var row    = options.row, cnt = 0, planes = [];
-    var numRow = row ? row : Math.sqrt(grid.length) - 1;
+    var cnt    = 0, planes = [];
+    var numPts = options.row;
+    var numRow = points.length/numPts;
 
-    for (var i = numRow; i > 0; i--) {
-        for (var j = numRow; j > 0; j--) {
+    for (var i = numRow - 1; i > 0; i--) {
+        for (var j = numPts - 1; j > 0; j--) {
 
-            var p1 = j + i * (numRow + 1), p4 = p1 - 1, p2 = p4 - numRow, p3 = p2 - 1;
+            var p1 = j + i * numRow, p4 = p1 - 1, p2 = p4 - numRow + 1, p3 = p2 - 1;
             var pl = [points[p1], points[p2], points[p3], points[p4]];
 
             pl.id       = 'plane_' + cnt++;
@@ -20,5 +21,6 @@ export function gridPlane(grid, options, point, angles){
             planes.push(pl);
         }
     }
+
     return planes;
 }
--- test/grid-planes-test.js
+++ test/grid-planes-test.js
@@ -1,7 +1,7 @@
 var tape = require('tape');
 var d3   = require('../');
 
-tape('test if the number of planes per row are calculated correct', function(test){
+tape('test if the number of planes per row are calculated correct when it\'s a quadratic grid', function(test){
 
     var j = 10, points = [];
 
@@ -11,8 +11,24 @@ tape('test if the number of planes per row are calculated correct', function(tes
         }
     }
 
-    var grid3d = d3._3d().shape('GRID').x(function(d){ return d.x; }).y(function(d){ return d.y; }).z(function(d){ return d.z; })(points);
+    var grid3d = d3._3d().shape('GRID', j*2).x(function(d){ return d.x; }).y(function(d){ return d.y; }).z(function(d){ return d.z; })(points);
     var row    = Math.sqrt(points.length) - 1;
     test.equal(grid3d.length, row*row);
+    test.end();
+});
+
+tape.only('test if the number of planes per row are calculated correct when it\'s not a quadratic grid', function(test){
+
+    var points = [], pointsPerRow = 5;
+
+    for (var z = 0; z < 3; z++) {
+        for (var x = 0; x < 5; x++) {
+            points.push({x: x, y: 1, z: z});
+        }
+    }
+    var grid3d = d3._3d().shape('GRID', pointsPerRow).x(function(d){ return d.x; }).y(function(d){ return d.y; }).z(function(d){ return d.z; })(points);
+
+    test.equal(8, grid3d.length);
+
     test.end();
 });
