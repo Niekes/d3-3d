eac1eb2e91f9b26c22e950c668c4e87ea939751e
niekes <info@niekes.com>
2017-09-10 18:34:57 +0200

    update version

--- src/draw/drawTriangle.js
+++ src/draw/drawTriangle.js
@@ -1,3 +1,9 @@
 export function drawTriangle(d){
-	return 'M' + d[0].projected.x + ',' + d[0].projected.y + 'L' + d[1].projected.x + ',' + d[1].projected.y + 'L' + d[2].projected.x + ',' + d[2].projected.y + 'Z';
+	return "M" + d[0].projected.x +
+         "," + d[0].projected.y +
+         "L" + d[1].projected.x +
+         "," + d[1].projected.y +
+         "L" + d[2].projected.x +
+         "," + d[2].projected.y +
+         "Z";
 }
