ecc1c332ddc45c258dc9c9da66fd2b47dc0dcc1d
Niekes <s@niekes.com>
2023-12-16 18:18:48 +0000

    Add test for sort function

---
+++ tests/sort-test.js
@@ -0,0 +1,26 @@
+import { test } from 'tape';
+import { sort } from '../src/sort.js';
+
+test.only('Sort function works properly', (t) => {
+    t.deepEqual(sort({ centroid: { z: 1 } }, { centroid: { z: 10 } }), -1);
+    t.deepEqual(sort({ centroid: { z: 10 } }, { centroid: { z: 1 } }), 1);
+    t.deepEqual(sort({ centroid: { z: 1 } }, { centroid: { z: 1 } }), 0);
+    t.deepEqual(sort({ centroid: { z: 1 } }, { centroid: { z: '1' } }), 0);
+    t.deepEqual(
+        isNaN(
+            sort(
+                {
+                    centroid: {
+                        z: function () {
+                            return false;
+                        }
+                    }
+                },
+                { centroid: { z: false } }
+            )
+        ),
+        true
+    );
+
+    t.end();
+});
