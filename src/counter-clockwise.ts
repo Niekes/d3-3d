fae22b387d42deaefbfe69a69e839027aebe6713
Stefan Nieke <s@niekes.com>
2026-01-05 17:42:06 +0000

    Migrate counter-clokcwise to ts

---
+++ src/counter-clockwise.ts
@@ -0,0 +1,32 @@
+export interface RotatedPoint {
+    x: number;
+    y: number;
+    z: number;
+}
+
+export interface PolygonPoint {
+    rotated?: RotatedPoint;
+}
+
+export function ccw(polygon: ReadonlyArray<PolygonPoint>): boolean {
+    const poly = [...polygon, polygon[0]];
+
+    let sum = 0;
+
+    for (let i = 0; i < polygon.length; i += 1) {
+        const p1 = poly[i].rotated;
+        const p2 = poly[i + 1].rotated;
+
+        if (p1 === undefined || p2 === undefined) {
+            throw new Error(
+                `ccw: polygon point at index ${
+                    p1 === undefined ? i : i + 1
+                } is missing rotated coordinates`
+            );
+        }
+
+        sum += (p2.x - p1.x) * (p2.y + p1.y);
+    }
+
+    return sum > 0;
+}
